889521891b4cf0c3c3f43b84eb637aceea42cddc
durmazertugrul <durmazer.tugrul.42@gmail.com>
2026-09-14 21:51:39 +0300

    feat: add player and enemy movement systems.

---
+++ Assets/Sprites/EnemyMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 3f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Boundary") 
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z); // Move the enemy down by 1 unit when hitting a boundary
+            moveSpeed *= -1; //reverse the direction of movement when hitting a boundary
+
+        }
+    }
+}
